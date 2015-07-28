651c715e85d4f04915f8bbac76b3661fe33b071c
st8ingenious <29981v4>
2015-07-28 22:31:23 +0300

    update, "resolve Ip" and "extract as xls" added
    resolve ip using ipinfo.io/
    extract table by kayalshri

--- upload/visits/index.php
+++ upload/visits/index.php
@@ -45,6 +45,10 @@ border:1px solid black;
 </style>
 </head>
 <body class="wo">
+<script type="text/javascript" src="jquery-1.3.1.min.js" ></script>
+<script type="text/javascript" src="tableExport.js"></script>
+<script type="text/javascript" src="jquery.base64.js"></script>
+
 <h1>Visits log</h1>
 <?php
 $link=Conection();
@@ -73,24 +77,96 @@ else{
 <a href="index.php?erase=true">! ! CLEAR LOG ! !</a>
 </br>
 </br>
+<a href="index.php?resolve=true">Resolve IP addresses</a>
+</br>(last 1000 visits)
+</br>
+</br>
+<input value="Export as xls" type="button" onclick="$('#exttable').tableExport({type:'excel', escape:'false'})">
+</br>
+</br>
 	<?php
    $link=Conection();
    $result=mysql_query("select * from visits order by id desc ",$link);
-   echo "<table style=\"display: inline-block; border: 1px solid; vertical-align: top; \">\n
+   
+   echo "<table id=\"exttable\" style=\"display: inline-block; border: 1px solid; vertical-align: top; \">\n
    <caption>Visitors</caption>
 
+
 <tr>
-<th>Page</th>
-<th>Date-time</th>
-<th>IP</th>
+<td>Page</td>
+<td>Date-time</td>
+<td>Visitors IP</td>
 </tr>";
+$apireq=0;
 
 while($row = mysql_fetch_array($result))
   {
+	  $ip= $row["ipcl"];
+	  if (isset($_GET["resolve"]) && ($_GET["resolve"]=="true")){
+	  if ($apireq<1000){
+	 $apireq=$apireq+1;
+	 
+	  
+
+$host1 = 'http://ipinfo.io/'. $ip . '/region';
+$host2 = 'http://ipinfo.io/'. $ip . '/city';
+$host3 = 'http://ipinfo.io/'. $ip . '/country';
+	
+	$city = null;
+	$country = null;
+	$region== null;
+	
+	
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $host1);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$region = curl_exec($ch);
+			curl_close ($ch);		
+		
+		    $ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $host2);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$city = curl_exec($ch);
+			curl_close ($ch);		
+			
+			
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $host3);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$country = curl_exec($ch);
+			curl_close ($ch);		
+		
+		$ipsa = "<b>IP:</b> {$ip} <br />\n";
+		$region=preg_replace( '/[^[:print:]]/', '',$region);
+		$city=preg_replace( '/[^[:print:]]/', '',$city);
+		$country=preg_replace( '/[^[:print:]]/', '',$country);
+		
+		if ($region !== 'undefined'){
+	    $ipsb = "<b>Region:</b> <span style='color:blue'>{$region}</span> | \n";
+		}else{$ipsb=null;}
+		
+		if ($city !== 'null'){
+		$ipsc = "<b>City:</b> <span style='color:blue'>{$city}</span> | \n";
+		}else{$ipsc=null;}
+		
+        $ipsd = "<b>Country:</b> <span style='color:blue'>{$country}</span> \n";		
+  } else{
+        $ipsa = "<b>IP:</b> {$ip}\n";
+	    $ipsb = null;
+		$ipsc = null;
+        $ipsd = null;}
+		
+		
+  } else{
+        $ipsa = "<b>IP:</b> {$ip}\n";
+	    $ipsb = null;
+		$ipsc = null;
+        $ipsd = null;}
+		
   echo "<tr>";
   echo "<td>" . $row["data"] . "</td>";
    echo "<td>" . $row["time"] . "</td>";
-    echo "<td>" . $row["ipcl"] . "</td>";
+    echo "<td>" . $ipsa . $ipsb . $ipsc . $ipsd . "</td>";
   echo "</tr>";
   }
 echo "</table>";
